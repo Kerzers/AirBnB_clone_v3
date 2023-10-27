a7f1ff375557387fce5ea61155d426646e8bca8e
kerzers <ahra.ahlame@gmail.com>
2023-10-27 11:59:13 -0700

    task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,6 +5,7 @@ from flask import Flask
 from models import storage
 from api.v1.views import app_views
 import os
+from flask import jsonify
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -13,6 +14,12 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def teardown(exception):
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    error = {"error": "Not found"}
+    return jsonify(error)
 
 
 if __name__ == "__main__":
