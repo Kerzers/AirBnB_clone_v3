4632a6e26b0ab53f567b84d48fa50c7d9175dff4
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-10-28 15:44:34 +0100

    task 6

---
+++ api/v1/views/states.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+
+from api.v1.views import app_views
+from models import storage
+from models.state import State
+from flask import jsonify, abort
+from flask import request
+
+
+@app_views.route("/states", methods=["GET"], strict_slashes=False)
+def states():
+    dict_states = storage.all(State)
+    list_states = []
+    for state in dict_states.values():
+        list_states.append(state.to_dict())
+    return list_states
+
+@app_views.route("/states/<id>", methods=["GET"], strict_slashes=False)
+def states_id(id):
+    dict_states = storage.all(State)
+    for key in dict_states.keys():
+        if key.split(".")[1] == id:
+            return jsonify(dict_states[key].to_dict())
+    abort(404)
+
+@app_views.route("/states/<id>", methods=["DELETE"], strict_slashes=False)
+def del_state(id):
+    dict_states = storage.all(State)
+    for key in dict_states.keys():
+        if key.split(".")[1] == id:
+            return {}
+    abort(404)
+
+@app_views.route("/states/", methods=["POST"], strict_slashes=False)
+def create_state():
+    try:
+        response = request.get_json()
+    except ValueError:
+        return jsonify({"Not a JSON"}), 400
+    if 'name' not in response:
+        return jsonify({"Missing name"}), 400
+
+    new_state = State(name=response['name'])
+    storage.new(new_state)
+    storage.save()
+
+    return jsonify(new_state.to_dict()), 201
+
+@app_views.route("/states/<id>", methods=["PUT"], strict_slashes=False)
+def update_state(id):
+    dict_states = storage.all(State)
+    
+    state = None
+    for key in dict_states.keys():
+        if key.split(".")[1] == id:
+            state = dict_states[key]
+            break
+
+    if state is None:
+        abort(404)
+
+    try:
+        response = request.get_json()
+    except ValueError:
+        return jsonify({"Not a JSON"}), 400
+
+    for key, value in response.items():
+        setattr(state, key, value)
+
+    storage.save()
+
+    return jsonify(state.to_dict()), 200
