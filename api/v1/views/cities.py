dbb3505335d6b5d70d07033a40b02e382a2443f2
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-10-29 17:21:34 +0100

    fix cities

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -6,6 +6,7 @@ from models.state import State
 from models.city import City
 from flask import jsonify, abort, request
 
+
 @app_views.route("/states/<id>/cities", methods=["GET"], strict_slashes=False)
 def cities(id):
     list_cities = []
@@ -17,12 +18,14 @@ def cities(id):
 
     return jsonify(list_cities)
 
+
 @app_views.route("/cities/<id>", methods=["GET"], strict_slashes=False)
 def city_id(id):
     city = storage.get(City, id)
     if city is None:
         abort(404)
     return jsonify(city.to_dict())
+
 
 @app_views.route("/cities/<id>", methods=["DELETE"], strict_slashes=False)
 def del_city(id):
@@ -33,7 +36,9 @@ def del_city(id):
     storage.save()
     return jsonify({}), 200
 
-@app_views.route("/states/<state_id>/cities", methods=["POST"], strict_slashes=False)
+
+@app_views.route("/states/<state_id>/cities", methods=["POST"],
+                 strict_slashes=False)
 def create_city(state_id):
     state = storage.get(State, state_id)
     if state is None:
@@ -51,6 +56,7 @@ def create_city(state_id):
     new_city.save()
 
     return jsonify(new_city.to_dict()), 201
+
 
 @app_views.route("/cities/<id>", methods=["PUT"], strict_slashes=False)
 def update_city(id):
